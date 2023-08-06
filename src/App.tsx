f082c37fc6b2d39bec5ea886c1067390c077a6ed
AJ Wells <wells19d@gmail.com>
2023-08-05 22:09:30 -0500

    setup header and nav bar locations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import NavBar from './Components/NavBar';
 
-function App() {
-  return <></>;
-}
+interface AppProps {}
+
+const App = (props: AppProps) => {
+  // const {} = props;
+
+  return (
+    <div className="container-fluid-0">
+      <div className="row m-0">
+        <div className="col-12 p-0">
+          <Header />
+        </div>
+        <div className="col-12 p-0">
+          <NavBar />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface NavBarProps {}
+
+const NavBar = (props: NavBarProps) => {
+  //   const {} = props;
+
+  return <div>NavBar</div>;
+};
+
+export default NavBar;
