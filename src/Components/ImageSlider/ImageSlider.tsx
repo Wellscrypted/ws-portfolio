4dcb4ca78f82ec225b1e089e1aa8d019458187d0
AJ Wells <wells19d@gmail.com>
2023-08-13 14:20:37 -0500

    completed slider for home screen

--- src/Components/ImageSlider/ImageSlider.tsx
+++ src/Components/ImageSlider/ImageSlider.tsx
@@ -4,12 +4,14 @@ import './SliderStyles.css';
 
 interface ImageSliderProps {
   slides: any;
+  timer?: number;
 }
 
 const ImageSlider = (props: ImageSliderProps) => {
-  const { slides } = props;
+  const { slides, timer } = props;
   const timerRef = useRef<any>(null);
   const [currentIndex, setCurrentIndex] = useState<any>(0);
+  const [sliderTime, setSliderTime] = useState<any>(0);
 
   const prev = () => {
     setCurrentIndex(currentIndex === 0 ? slides.length - 1 : currentIndex - 1);
@@ -23,46 +25,59 @@ const ImageSlider = (props: ImageSliderProps) => {
   };
 
   useEffect(() => {
+    if (timer) {
+      setSliderTime(timer * 1000);
+    } else {
+      setSliderTime(999999999);
+    }
+
     if (timerRef.current) {
       clearTimeout(timerRef.current);
     }
     timerRef.current = setTimeout(() => {
       next();
-    }, 5000);
+    }, sliderTime);
     return () => {
       clearTimeout(timerRef.current);
     };
-  }, [next]);
+  }, [next, timer, sliderTime]);
 
   return (
     <div className="slider-container">
-      {/* <div className="arrow-container-left" onClick={prev}>
-        <div className="arrow-button">
-          <IconListRouter.LeftArrowIcon style={{ fill: '#ffe500' }} size={'30px'} />
-        </div>
-      </div> */}
-      {/* <div className="arrow-container-right" onClick={next}>
-        <div className="arrow-button">
-          <IconListRouter.RightArrowIcon style={{ fill: '#ffe500' }} size={'30px'} />
-        </div>
-      </div> */}
       <div
         className="slider-style"
         style={{
           backgroundImage: `url(${slides[currentIndex].url})`,
         }}
       >
-        <div className="container-fluid-0" style={{ border: 'solid black 1px', height: '100%' }}>
-          <div className="row m-0" style={{ border: 'solid red 1px', height: '100%' }}>
-            <div className="col-1" style={{ border: 'solid green 1px', height: '100%' }}>
+        <div className="container-fluid-0 full-height">
+          <div className="row m-0 full-height">
+            <div className="col-1 center-v-align">
               <div className="arrow-container-left" onClick={prev}>
                 <div className="arrow-button">
                   <IconListRouter.LeftArrowIcon style={{ fill: '#ffe500' }} size={'30px'} />
                 </div>
               </div>
             </div>
-            <div className="col-10" style={{ border: 'solid green 1px', height: '100%' }}></div>
-            <div className="col-1" style={{ border: 'solid green 1px', height: '100%' }}></div>
+            <div className="col-10 center-vh-align">
+              <div className="slide-effect">
+                <div className="row m-0">
+                  <div className={`col-12 p-0 text-center fs-24 text-${currentIndex} ft-bold`}>
+                    {slides[currentIndex].header}
+                  </div>
+                  <div className={`col-12 p-0 text-center slideUp-${currentIndex} ft-reg`}>
+                    {slides[currentIndex].content1}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-1 center-v-align right-align">
+              <div className="arrow-container-right" onClick={next}>
+                <div className="arrow-button">
+                  <IconListRouter.RightArrowIcon style={{ fill: '#ffe500' }} size={'30px'} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -14,25 +14,23 @@ const Home = () => {
     {
       url: Image1,
       alt: 'Image1',
-      header: 'Scheduled Projects',
-      content1:
-        'Sitting down with a team, helping layout the project details, and working hard to keep us on schedule!',
+      header: 'UI/UX Designs',
+      content1: `Creating beautiful and functional designs using Bootstrap, Material-UI, Adobe Photoshop, Adobe Illustrator, and other custom library's.`,
       content2: '',
     },
     {
       url: Image2,
       alt: 'Image2',
-      header: 'React Designs',
-      content1:
-        'I really love working on Front-End development. Im looking forward towards a future in Native React Development.',
+      header: 'Responsive & Cross-Platform Capabilities',
+      content1: `I really enljy working with React Native for iOS and Android applications. Currently working on expanding my knowledge for React Native for Web.`,
       content2: '',
     },
     {
       url: Image3,
       alt: 'Image2',
-      header: 'Professional Approach',
+      header: 'Front-End Developer',
       content1:
-        'Whatever type of development is about to launch, I enjoy working with the skills Ive learned, while learning new ones for future development projects!',
+        'Helping to create a better user experience through the use of React, React Native, HTML, CSS, JavaScript, and TypeScript.',
       content2: '',
     },
   ];
@@ -48,7 +46,7 @@ const Home = () => {
             : 'slider-container-style-lg'
         }
       >
-        <ImageSlider slides={slides} />
+        <ImageSlider slides={slides} timer={7} />
       </div>
     </div>
   );
